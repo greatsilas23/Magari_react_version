41d547a6e35dbe3684aae263119f0a17f5eee53a
greatsilas23 <83508822+greatsilas23@users.noreply.github.com>
2023-05-16 11:30:42 +0300

    Add files via upload

---
+++ Aside.js
@@ -0,0 +1,16 @@
+import {React, useState, useEffect} from 'react'
+const Aside = ({props})=> {
+    const [itemLogo, setItemLogo] = useState(new Array())
+    const [isLoaded, setIsLoaded] = useState(false)
+    useEffect(() => setItemLogo(itemLogo.push(props.itemLogo)).then(setIsLoaded(true)), [])
+    return(
+        <div>
+
+        
+        {isLoaded
+             && itemLogo.map((sector) => <section></section>)
+        }
+        </div>
+    )
+}
+export default Aside
